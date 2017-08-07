6907c414f387cfb0fc3304295deefb8bd5d34f24
Carlos Perez <caperez@blizzard.com>
2017-08-07 15:41:28 -0700

    set up react routes

---
+++ app/config/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "react-router";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Main from "../components/Main";
+
+// not sure if you need these. try without. 
+var hashHistory = router.hashHistory; 
+var IndexRoute = router.IndexRoute;
+
+export default (
+    <Router>
+        <Route path="/" component={Main}></Route>
+    </Router>
+)
---
+++ app/utils/helpers.js
@@ -0,0 +1,9 @@
+import react from "react";
+import router from "react-router";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Main from "../components/Main";
+
+// not sure if you need these. try without. 
+var hashHistory = router.hashHistory; 
+var IndexRoute = router.IndexRoute;
+
